933f94951163da08712d66785ddb9f09e9df75d9
CyrilFauveau <fauveaucyril@gmail.com>
2020-11-03 09:57:06 +0100

    first commit

---
+++ Exercice_1/Exercice_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var nb = rnd.Next(100) + 1;
+
+            Console.WriteLine(nb);
+            Console.WriteLine("Saisissez un nombre entre 1 et 100");
+
+            while (true)
+            {
+                int value = Convert.ToInt32(Console.ReadLine());
+
+                if (value < nb)
+                {
+                    Console.WriteLine("Plus grand");
+                }
+                else if (value > nb)
+                {
+                    Console.WriteLine("Plus petit");
+                }
+                else if (value == nb)
+                {
+                    Console.WriteLine("Gagné");
+                    break;
+                }
+            }
+        }
+    }
+}
