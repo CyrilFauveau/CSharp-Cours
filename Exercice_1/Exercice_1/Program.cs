302609ed100d97583c1dcd947828b1c969dcb8ac
CyrilFauveau <fauveaucyril@gmail.com>
2020-11-03 11:33:27 +0100

    upd ex

--- Exercice_1/Exercice_1/Program.cs
+++ Exercice_1/Exercice_1/Program.cs
@@ -7,7 +7,7 @@ namespace Exercice_1
     {
         static void Main(string[] args)
         {
-            var game = new Game();
+            var game = new AdvancedGame(5);
             game.Play();
         }
     }
@@ -18,35 +18,74 @@ namespace Exercice_1
 
         public Game(int maxValue)
         {
-            var _randomValue = new Random().Next(maxValue) + 1;
+            _randomValue = new Random().Next(maxValue) + 1;
         }
 
         public Game(): this(100) {}
 
         public void Play()
         {
-            Console.WriteLine("Saisissez un nombre entre 1 et 100");
+            int value = 0;
 
-            bool win = false;
-
-            while (!win)
+            do
             {
-                int value = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Saisissez un nombre entre 1 et 100");
+                value = Convert.ToInt32(Console.ReadLine());
+            } while (!Win(value));
+        }
 
-                if (value < _randomValue)
+        protected virtual bool Win(int value)
+        {
+            if (value < _randomValue)
+            {
+                Console.WriteLine("Plus grand");
+            }
+            else if (value > _randomValue)
+            {
+                Console.WriteLine("Plus petit");
+            }
+            else
+            {
+                Console.WriteLine("Gagné");
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class AdvancedGame : Game
+    {
+        private readonly int _maxTrials;
+        private int _actualTrials;
+
+        public AdvancedGame(int maximumTrials): base()
+        {
+            _maxTrials = maximumTrials;
+        }
+
+        protected override bool Win(int value)
+        {
+            if (value == -1)
+            {
+                return true;
+            }
+
+            if (!base.Win(value))
+            {
+                int _leftTrials = _maxTrials - _actualTrials - 1;
+
+                Console.WriteLine("Il vous reste " + _leftTrials + " essais");
+                if (++_actualTrials == _maxTrials)
                 {
-                    Console.WriteLine("Plus grand");
-                }
-                else if (value > _randomValue)
-                {
-                    Console.WriteLine("Plus petit");
-                }
-                else if (value == _randomValue)
-                {
-                    Console.WriteLine("Gagné");
-                    win = true;
+                    Console.WriteLine("Vous avez perdu");
+                    return true;
                 }
             }
+            else
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
